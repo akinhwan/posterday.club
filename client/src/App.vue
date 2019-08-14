8b73e2c8e63248544d4a3c89b53413553ba0dd25
Andrew Kim <akinhwan@gmail.com>
2019-08-13 23:01:51 -0400

    add youtube layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,36 +1,38 @@
 <template>
   <v-app>
-    <v-app-bar app>
+    <!-- <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
         <span class="mr-2">Latest Release</span>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar>-->
 
     <v-content>
-      <HelloWorld/>
+      <!-- <HelloWorld/> -->
+      <div id="nav">
+        <router-link to="/">Home</router-link>|
+        <router-link to="/about">About</router-link>|
+        <router-link to="/dashboard">Dashboard</router-link>
+      </div>
+      <router-view />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    HelloWorld
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,25 @@
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <div id="home">
+    <h1>hello</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
+// import HelloWorld from '../components/HelloWorld';
 
 export default {
   components: {
-    HelloWorld,
-  },
+    // HelloWorld
+  }
 };
 </script>
+<style lang="scss" scoped>
+div {
+  // background-color: blue;
+
+  h1 {
+    // font-size: 10rem;
+  }
+}
+</style>
